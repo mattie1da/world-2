f185303f3d0965dd96741379cce14fda60e79cdc
Matt Wyatt <hello@mattie.world>
2022-07-06 12:12:27 +0100

    Icon components

---
+++ components/icons/IconStrava.tsx
@@ -0,0 +1,21 @@
+export const IconStrava = () => {
+  return (
+    <svg
+      width="29"
+      height="29"
+      viewBox="0 0 29 29"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="29" height="29" rx="2" fill="currentColor" />
+      <path
+        d="M13.2773 4L7 15.5429H10.6984L13.2773 10.9534L15.8354 15.5429H19.5048L13.2773 4Z"
+        fill="currentColor"
+      />
+      <path
+        d="M18.8125 15.5429L17.1136 19.0136L15.3876 15.5429H12.7715L17.1136 24.2L21.4286 15.5429H18.8125Z"
+        fill="currentColor"
+        fillOpacity="0.7"
+      />
+    </svg>
+  );
+};
---
+++ components/icons/index.ts
@@ -0,0 +1,1 @@
+export { IconStrava } from './IconStrava';
